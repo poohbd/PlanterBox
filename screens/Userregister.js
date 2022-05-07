0458bafdbb4e48f687da66f14508a6fcc675e2ef
chimauto <62636000+chimauto@users.noreply.github.com>
2022-05-08 01:01:22 +0700

    login cloud

--- screens/Userregister.js
+++ screens/Userregister.js
@@ -51,7 +51,7 @@ export default Userregister = ({navigation}) => {
         const {email,password,username} = values;
         const config = {
           method: 'POST',
-          url: 'http://192.168.1.42:3000/user/register',
+          url: 'http://my-app-dpydq.ondigitalocean.app/user/register',
           data: {
             email: email,
             username: username,
